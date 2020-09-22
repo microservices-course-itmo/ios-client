0382736e05ed997e79c7fa33267152a2dd810f4e
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2020-09-22 23:09:30 +0300

    Created base SwiftUI project with UI and Unit tests

---
+++ WineUp/WineUp/WineUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WineUpApp.swift
+//  WineUp
+//
+//  Created by Александр Пахомов on 22.09.2020.
+//
+
+import SwiftUI
+
+@main
+struct WineUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
