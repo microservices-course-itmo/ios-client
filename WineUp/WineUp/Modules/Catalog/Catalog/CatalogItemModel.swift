1178ea198a53d70402717b20d8ae27c5d25a69e4
George <40645827+PhotolocaDeveloper@users.noreply.github.com>
2020-09-28 21:45:22 +0300

    Create ui in catalog row view (#9)
    * WIP
    
    * Design improvements
    
    * Design improvement and fixes
    
    * Delete project.pbxproj
    
    Removed file "project.pbxproj" from repository due to Xcodegen usage
    
    * Fixes according to mr
    
    Co-authored-by: George Lyurko <geo201213ua@gmail.com>
    Co-authored-by: BobCatC <31915855+BobCatC@users.noreply.github.com>

--- WineUp/WineUp/Modules/Catalog/Catalog/CatalogItemModel.swift
+++ WineUp/WineUp/Modules/Catalog/Catalog/CatalogItemModel.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Rswift
 
 final class CatalogItemModel: Identifiable, CustomStringConvertible {
     var id: UUID
@@ -49,20 +50,31 @@ final class CatalogItemModel: Identifiable, CustomStringConvertible {
         self.discountPercents = discountPercents
     }
 
+    // TODO: Rewrite this method with buissnes logic aspects
+
+    var priceWithDiscount: Float {
+        let result = self.originalPriceRub * ((100 - self.discountPercents) / 100)
+        return Float(result)
+    }
+
+    var wineDescription: String {
+        return "\(country), \(color.name), \(wineAstringency.name), \(quantityLiters) л"
+    }
+
     // TODO: Must be removed after test data is imported into project
     convenience init(title: String) {
         self.init(title: title,
-                  country: "",
+                  country: "Украина",
                   color: .red,
                   wineAstringency: .dry,
-                  quantityLiters: 0,
+                  quantityLiters: 2,
                   isLiked: false,
-                  chemistry: 0,
-                  titleImage: UIImage(),
-                  retailerImage: UIImage(),
-                  rating: 0,
-                  originalPriceRub: 0,
-                  discountPercents: 0)
+                  chemistry: 3,
+                  titleImage: UIImage(named: "Red_and_white_wine_12-2015")!,
+                  retailerImage: UIImage(named: "image 4")!,
+                  rating: 3,
+                  originalPriceRub: 1_400,
+                  discountPercents: 20)
     }
 
     var description: String {
@@ -72,8 +84,36 @@ final class CatalogItemModel: Identifiable, CustomStringConvertible {
 
 enum WineColor {
     case red, white, rose
+
+    // TODO: Create localize file with localization, can use Rswift for accessing it
+
+    var name: String {
+        switch self {
+        case .red:
+            return "Красное"
+        case .white:
+            return "Белое"
+        case .rose:
+            return "Розовое"
+        }
+    }
 }
 
 enum WineAstringency {
     case dry, semiDry, semiSweet, sweet
+
+    // TODO: Create localize file with localization, can use Rswift for accessing it
+
+    var name: String {
+        switch self {
+        case .dry:
+            return "Сухое"
+        case .semiDry:
+            return "Полусухое"
+        case .semiSweet:
+            return "Полусладкое"
+        case .sweet:
+            return "Сладкое"
+        }
+    }
 }
---
+++ WineUp/WineUp/Modules/Catalog/Catalog/CatalogRow/CatalogRowInfoView.swift
@@ -0,0 +1,82 @@
+//
+//  CatalogRowInfoView.swift
+//  WineUp
+//
+//  Created by George on 25.09.2020.
+//
+
+import SwiftUI
+
+struct CatalogRowInfoView: View {
+
+    // MARK: - Private Properties
+
+    let item: CatalogItemModel
+
+    // MARK: - Lifecycle
+
+    init(item: CatalogItemModel) {
+        self.item = item
+    }
+
+    // MARK: - View
+
+    var body: some View {
+        VStack {
+            HStack {
+                Text(item.title)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                    .lineLimit(4)
+                Spacer()
+                Image(systemName: "suit.heart.fill")
+                    .foregroundColor(getHeartColor())
+                    .font(.system(size: 25))
+            }
+            Spacer()
+            HStack {
+                Text(item.wineDescription)
+                    .font(.system(size: 13))
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(4)
+                Spacer()
+            }
+            Spacer()
+            HStack {
+                Spacer()
+                // TODO: Add percent to CatalogItemModel
+                Text("Подходит вам на \(Int(item.chemistry))%")
+                    .font(.system(size: 11))
+            }
+            Spacer()
+            HStack {
+                Image(uiImage: item.retailerImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 80)
+                    .padding(.leading, 8)
+                Spacer()
+                CatalogRowDiscountView(item: item)
+            }
+        }
+    }
+}
+
+// MARK: - View Methods
+
+extension CatalogRowInfoView {
+    func getHeartColor() -> Color {
+        return item.isLiked ? .red : .gray
+    }
+}
+
+// MARK: - Preview Settings
+
+#if DEBUG
+struct CatalogRowInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        CatalogRowInfoView(item: CatalogItemModel(title: "ASD"))
+            .previewLayout(.fixed(width: 274, height: 130))
+    }
+}
+#endif
