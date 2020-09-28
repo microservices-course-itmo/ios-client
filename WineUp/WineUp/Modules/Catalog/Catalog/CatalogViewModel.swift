d7792a8eed7457aae5288642361344a585c25668
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2020-09-28 21:44:55 +0300

    Implemented CatalogFiltersBarView and added test data

--- WineUp/WineUp/Modules/Catalog/Catalog/CatalogViewModel.swift
+++ WineUp/WineUp/Modules/Catalog/Catalog/CatalogViewModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 final class CatalogViewModel: ObservableObject {
     @Published var items: [CatalogItemModel]
+    @Published var filtersBarItems: [CatalogFiltersBarItemModel]
 
     private static let testItems: [CatalogItemModel] = [
         CatalogItemModel(title: "1"),
@@ -17,5 +18,16 @@ final class CatalogViewModel: ObservableObject {
 
     init() {
         self.items = CatalogViewModel.testItems
+        self.filtersBarItems = [
+            CatalogFiltersBarItemModel(title: "Рекомендованные"),
+            CatalogFiltersBarItemModel(title: "Цена"),
+            CatalogFiltersBarItemModel(title: "Страна"),
+            CatalogFiltersBarItemModel(title: "Цвет"),
+            CatalogFiltersBarItemModel(title: "Сахар")
+        ]
+    }
+
+    func filterItemDidTap(_ item: CatalogFiltersBarItemModel) {
+
     }
 }
