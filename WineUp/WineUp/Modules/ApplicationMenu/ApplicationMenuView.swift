5f9a1954f04ab45eff5688af206632cd95723dad
Vlad-9 <39233500+Vlad-9@users.noreply.github.com>
2020-09-29 11:29:05 +0300

    Added TabBar icons and test items (#10)

--- WineUp/WineUp/Modules/ApplicationMenu/ApplicationMenuView.swift
+++ WineUp/WineUp/Modules/ApplicationMenu/ApplicationMenuView.swift
@@ -12,10 +12,26 @@ struct ApplicationMenuView: View {
 
     var body: some View {
         TabView {
-            CatalogRootView()
-                .tabItem {
-                    Text("Catalog")
-                }
+         CatalogRootView()
+                 .tabItem {
+                     Image(systemName: "house.fill")
+                     Text("Главное")
+                 }
+         CatalogRootView()
+                 .tabItem {
+                     Image(systemName: "book.fill")
+                     Text("Каталог")
+                 }
+         CatalogRootView()
+                 .tabItem {
+                     Image(systemName: "star.fill")
+                     Text("Избранное")
+                 }
+         CatalogRootView()
+                 .tabItem {
+                     Image(systemName: "person.fill")
+                     Text("Профиль")
+                 }
         }
     }
 }
