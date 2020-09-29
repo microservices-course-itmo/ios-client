e3b772d40cb222db157ccd56a5a54a2ff492ef19
Vlad-9 <swebus999@gmail.com>
2020-09-29 03:39:03 +0300

    Added TabBar icons and test items

--- WineUp/WineUp/Modules/Catalog/Catalog/CatalogViewModel.swift
+++ WineUp/WineUp/Modules/Catalog/Catalog/CatalogViewModel.swift
@@ -6,15 +6,15 @@
 //
 
 import Foundation
+import UIKit.UIImage
 
 final class CatalogViewModel: ObservableObject {
     @Published var items: [CatalogItemModel]
 
     private static let testItems: [CatalogItemModel] = [
-        CatalogItemModel(title: "Placeholder test"),
-        CatalogItemModel(title: "test test test test"),
-        CatalogItemModel(title: "test test test test"),
-        CatalogItemModel(title: "test test test test")
+        CatalogItemModel(title: "Sarget du Gruaud Larose, AOC Saint-Julien", country: "France", color: .red, wineAstringency: .dry, quantityLiters: 0.75, isLiked: true, chemistry: 25, titleImage: UIImage(imageLiteralResourceName: "testWine 04"), retailerImage: UIImage(imageLiteralResourceName: "image 2"), rating: 3, originalPriceRub: 5_999, discountPercents: 0),
+        CatalogItemModel(title: "Luce Brunello Di Montalcino", country: "Italia", color: .red, wineAstringency: .dry, quantityLiters: 0.75, isLiked: false, chemistry: 15, titleImage: UIImage(imageLiteralResourceName: "testWine 02"), retailerImage: UIImage(imageLiteralResourceName: "image 4"), rating: 5, originalPriceRub: 14_878, discountPercents: 13),
+        CatalogItemModel(title: "Footsteps Reserve Zinfandel", country: "USA", color: .red, wineAstringency: .semiDry, quantityLiters: 0.75, isLiked: false, chemistry: 30, titleImage: UIImage(imageLiteralResourceName: "testWine 05"), retailerImage: UIImage(imageLiteralResourceName: "image 3"), rating: 4, originalPriceRub: 1_799, discountPercents: 5)
     ]
 
     init() {
