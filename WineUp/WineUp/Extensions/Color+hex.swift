ade29e5be60cab3a16ec4ec2806eb02f543ca9ba
dimonker <rebrik.dmitry@gmail.com>
2020-10-04 14:19:51 +0300

    UI-6 recommendation filter

---
+++ WineUp/WineUp/Extensions/Color+hex.swift
@@ -0,0 +1,20 @@
+//
+//  Color+hex.swift
+//  WineUp
+//
+//  Created by Dmitry Rebrik on 04.10.2020.
+//
+
+import SwiftUI
+
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
