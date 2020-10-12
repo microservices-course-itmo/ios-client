a1bb8146e57fc18831632813e27fdf18f9d2cc02
Nikolai Solonenko <70380928+nsolonenko@users.noreply.github.com>
2020-10-12 12:17:33 +0300

    [UI-9-verification-view] add verification view (#18)
    * UI-9-verification-view add few elements
    
    * UI-90-verification-view add buttons
    
    * UI-9-varification-view pr fix
    
    * UI-9-verification-code fix
    
    * UI-9-verification-code pr fix 2

---
+++ WineUp/WineUp/Modules/Authorization/VerificationCodeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  VerificationCodeViewModel.swift
+//  WineUp
+//
+//  Created by Nikolai Solonenko on 11.10.2020.
+//
+
+import Foundation
+
+final class VerificationCodeViewModel: ObservableObject {
+    @Published var code: String = ""
+}
