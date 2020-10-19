c88d496f401fcbb5a7b46715ec41dcce048a1bd0
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2020-10-19 14:42:58 +0300

    Created extension View.anyView

---
+++ WineUp/WineUp/Extensions/View+anyView.swift
@@ -0,0 +1,15 @@
+//
+//  View+anyView.swift
+//  WineUp
+//
+//  Created by Александр Пахомов on 19.10.2020.
+//
+
+import SwiftUI
+
+extension View {
+    var anyView: AnyView {
+        AnyView(self)
+    }
+}
+
