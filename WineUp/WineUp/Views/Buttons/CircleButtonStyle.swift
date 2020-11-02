056db49344a5c003de196f0362bbfa469e05e49a
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2020-11-02 12:59:51 +0300

    UI-18: Wrapped suggestions list in Pager and implemented paging logic

--- WineUp/WineUp/Views/Buttons/CircleButtonStyle.swift
+++ WineUp/WineUp/Views/Buttons/CircleButtonStyle.swift
@@ -10,30 +10,48 @@ import SwiftUI
 // MARK: - Constants
 
 private extension Color {
-    static let discountColor = Color(red: 145 / 255, green: 22 / 255, blue: 52 / 255)
+    static let enabledTitle: Color = .white
+    static let enabledBackground = Color(red: 145 / 255, green: 22 / 255, blue: 52 / 255)
+    static let disabledTitle: Color = .white
+    static let disabledBackground = Color(.systemGray4)
 }
 
 // MARK: - ButtonStyle
 
 struct CircleButtonStyle: ButtonStyle {
 
-    let color: Color
+    let isDisabled: Bool
 
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .frame(width: 75, height: 75)
-            .foregroundColor(Color.white)
-            .background(color)
+            .frame(width: 70, height: 70)
+            .foregroundColor(titleColor)
+            .background(backgroundColor)
             .clipShape(Circle())
+            .scaleEffect(configuration.isPressed ? 0.98 : 1)
+            .overlay(configuration.isPressed ? Color.white.opacity(0.15) : Color.clear)
+            .animation(.linear(duration: 0.2), value: configuration.isPressed)
+            .animation(.linear(duration: 0.1), value: isDisabled)
     }
 }
 
 // MARK: - Helpers
 
+private extension CircleButtonStyle {
+    var titleColor: Color {
+        isDisabled ? .disabledTitle : .enabledTitle
+    }
+
+    var backgroundColor: Color {
+        isDisabled ? .disabledBackground : .enabledBackground
+    }
+}
+
 extension Button {
-    func circleStyled(color: Color = .discountColor) -> some View {
+    func circleStyled(isDisabled: Bool = false) -> some View {
         self
-            .buttonStyle(CircleButtonStyle(color: color))
+            .buttonStyle(CircleButtonStyle(isDisabled: isDisabled))
+            .disabled(isDisabled)
     }
 }
 
@@ -42,9 +60,14 @@ extension Button {
 #if DEBUG
 struct CircleButtonStyle_Previews: PreviewProvider {
     static var previews: some View {
-        Button("Title", action: {})
-            .circleStyled()
-            .previewLayout(.fixed(width: 414, height: 120))
+        Group {
+            Button("Title", action: {})
+                .circleStyled(isDisabled: false)
+
+            Button("Title", action: {})
+                .circleStyled(isDisabled: true)
+        }
+        .previewLayout(.fixed(width: 414, height: 120))
     }
 }
 #endif
