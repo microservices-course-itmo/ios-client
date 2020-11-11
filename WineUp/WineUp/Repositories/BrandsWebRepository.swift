285b8a7ccd975d80ea5c9eb7cb43185148b67192
Nikolai Solonenko <70380928+nsolonenko@users.noreply.github.com>
2020-11-11 10:32:31 +0300

    NET-3-brands-repository create BrandsWebRepository and BrandJson (#37)
    * NET-3-brands-repository create BrandsWebRepository and BrandJson
    
    * NET-3-brands-repository pr fix
    
    * Minor fix of protocol method name
    
    Co-authored-by: Aleksandr Pakhomov <twofoursixeight@yandex.ru>

---
+++ WineUp/WineUp/Repositories/BrandsWebRepository.swift
@@ -0,0 +1,72 @@
+//
+//  BrandsWebRepository.swift
+//  WineUp
+//
+//  Created by Nikolai Solonenko on 08.11.2020.
+//
+
+import Foundation
+import Combine
+
+// MARK: BrandsWebRepository
+
+protocol BrandsWebRepository: WebRepository {
+    /// Create brand with data from form
+    func createBrand(from form: BrandJson.CreateForm) -> AnyPublisher<Void, Error>
+    /// Get list of all brands
+    func getAllBrands() -> AnyPublisher<[BrandJson], Error>
+    /// Update brand by Id
+    func updateBrand(by id: String, with form: BrandJson.UpdateForm) -> AnyPublisher<[BrandJson], Error>
+    /// Get brand by Id
+    func getBrandById(by id: String) -> AnyPublisher<BrandJson, Error>
+}
+
+// MARK: - Implementation
+
+final class RealBrandsWebRepository: BrandsWebRepository {
+
+    let session: URLSession
+    let baseURL: String
+    let bgQueue = DispatchQueue(label: "bg_parse_queue")
+
+    init(session: URLSession, baseURL: String) {
+        self.session = session
+        self.baseURL = baseURL
+    }
+
+    func createBrand(from form: BrandJson.CreateForm) -> AnyPublisher<Void, Error> {
+        call(endpoint: .createBrand(form))
+    }
+
+    func getAllBrands() -> AnyPublisher<[BrandJson], Error> {
+        request(endpoint: .getAllBrands())
+    }
+
+    func updateBrand(by id: String, with form: BrandJson.UpdateForm) -> AnyPublisher<[BrandJson], Error> {
+        request(endpoint: .updateBrand(by: id, with: form))
+    }
+
+    func getBrandById(by id: String) -> AnyPublisher<BrandJson, Error> {
+        request(endpoint: .getBrandById(by: id))
+    }
+}
+
+// MARK: - Helpers
+
+private extension APICall {
+    static func createBrand(_ form: BrandJson.CreateForm) -> APICall {
+        APICall(path: "/brand", method: "POST", headers: HTTPHeaders.empty.mockedAccessToken(), value: form)
+    }
+
+    static func updateBrand(by id: String, with form: BrandJson.UpdateForm) -> APICall {
+        APICall(path: "/brand/\(id)", method: "PUT", headers: HTTPHeaders.empty.mockedAccessToken())
+    }
+
+    static func getAllBrands() -> APICall {
+        APICall(path: "/brand", method: "GET", headers: HTTPHeaders.empty.mockedAccessToken())
+    }
+
+    static func getBrandById(by id: String) -> APICall {
+        APICall(path: "/brand/\(id)", method: "GET", headers: HTTPHeaders.empty.mockedAccessToken())
+    }
+}
