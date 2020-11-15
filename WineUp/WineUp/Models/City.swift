ed4f510e2c117ce8c7b772000c6f3e716605dba7
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2020-11-15 13:39:59 +0300

    Replaced TextField with Picker in city input view

---
+++ WineUp/WineUp/Models/City.swift
@@ -0,0 +1,32 @@
+//
+//  City.swift
+//  WineUp
+//
+//  Created by Александр Пахомов on 15.11.2020.
+//
+
+import SwiftUI
+
+enum City: Int, CaseIterable, Identifiable {
+    case notSelected = -1
+    case saintPetersburg = 1
+    case moscow = 2
+
+    var id: Int { rawValue }
+
+    static var displayCases: [City] {
+        allCases.removing(.notSelected)
+    }
+
+    var titleName: LocalizedStringKey {
+        switch self {
+        case .saintPetersburg:
+            return "Санкт-Петербург"
+        case .moscow:
+            return "Москва"
+        default:
+            assertionFailure()
+            return ""
+        }
+    }
+}
