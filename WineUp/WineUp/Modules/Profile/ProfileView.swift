d44adfd28104c984cc20a1910fe79d5c5d3fa040
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2021-02-16 12:19:01 +0300

    Impelemented simple UI of profile tab

--- WineUp/WineUp/Modules/Profile/ProfileView.swift
+++ WineUp/WineUp/Modules/Profile/ProfileView.swift
@@ -16,26 +16,71 @@ struct ProfileView: View {
 
     var body: some View {
         VStack(spacing: 32) {
-            Button(action: viewModel.logoutButtonDidTap, label: {
-                Text("Logout")
-            })
-            .defaultStyled(isDisabled: false)
+            Spacer()
+//            Spacer()
 
-            Button(action: {
-                self.showingAlert = true
-            }, label: {
-                Text("Show APNs Token")
-            })
-            .alert(isPresented: $showingAlert) {
-                Alert(
-                    title: Text("APNs Token (hex representation)"),
-                    message: Text("Hex representation: \(viewModel.hexAPNSId ?? "#error")"),
-                    primaryButton: .default(Text("Copy to clipboard"), action: {
-                        UIPasteboard.general.string = viewModel.hexAPNSId
-                    }),
-                    secondaryButton: .default(Text("OK"))
-                )
+            VStack(spacing: 0) {
+                Text("Иван Иванов")
+                    .font(.title)
+                    .padding()
+
+                VStack(alignment: .center, spacing: 0) {
+                    VStack(alignment: .leading) {
+                        Text("Номер телефона:")
+                            .foregroundColor(.gray)
+
+                        Text("+7 (911) 272-78-57")
+                            .frame(width: 260)
+                            .padding(.vertical, 16)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8.0)
+                    }
+                    .padding(.bottom)
+
+                    VStack(alignment: .leading) {
+                        Text("Город:")
+                            .foregroundColor(.gray)
+
+                        Text("Москва")
+                            .frame(width: 260)
+                            .padding(.vertical, 16)
+                            .background(Color(.systemGray6))
+                            .cornerRadius(8.0)
+
+                    }
+                    .padding(.bottom, 48)
+
+                    Button(action: viewModel.logoutButtonDidTap, label: {
+                        Text("Выйти из аккаунта")
+                    })
+                    .defaultStyled(isDisabled: false)
+                    .padding(.bottom, 32)
+
+                    Button(action: {
+                        self.showingAlert = true
+                    }, label: {
+                        Text("Show APNs Token")
+                    })
+                    .alert(isPresented: $showingAlert) {
+                        Alert(
+                            title: Text("APNs Token (hex representation)"),
+                            message: Text("Hex representation: \(viewModel.hexAPNSId ?? "#error")"),
+                            primaryButton: .default(Text("Copy to clipboard"), action: {
+                                UIPasteboard.general.string = viewModel.hexAPNSId
+                            }),
+                            secondaryButton: .default(Text("OK"))
+                        )
+                    }
+                }
+                .padding()
             }
+            .cardStyled()
+            .frame(maxHeight: 600)
+            .padding()
+
+
+            //
+            //
         }
     }
 }
