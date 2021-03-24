6e2b71410955f69ea62f69dcc64729359d8622d4
Aleksandr Pakhomov <twofoursixeight@yandex.ru>
2021-03-24 19:51:57 +0300

    NOT-1: Removed mock code

--- WineUp/WineUp/Modules/ApplicationMenu/ApplicationMenuView+ViewModel.swift
+++ WineUp/WineUp/Modules/ApplicationMenu/ApplicationMenuView+ViewModel.swift
@@ -24,15 +24,9 @@ extension ApplicationMenuView {
             cancelBag.collect {
                 container.appState.bind(\.routing.selectedTab, to: self, by: \.selectedTab)
                 $selectedTab.bind(to: container.appState, by: \.value.routing.selectedTab)
-            }
-
-            NotificationsService.shared.$wineId.sink {
-                self.loadWinePosition(with: $0)
-            }.store(in: cancelBag)
-
-            // эмулируем нажатие на уведомление и изменение переменной wineId сервиса через 2 секунды после запуска
-            DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(2)) {
-                NotificationsService.shared.wineId = "9355cf50-f669-4b40-9fe8-18ee4b46ed34"
+                NotificationsService.shared.$wineId.sink { [weak self] in
+                    self?.loadWinePosition(with: $0)
+                }
             }
         }
 
@@ -50,28 +44,25 @@ extension ApplicationMenuView {
             .init(container: container)
         }
 
-        var winePositionDetailsViewModel: WinePositionDetailsView.ViewModel? {
-            guard let winePosition = winePosition else { return nil }
-            return .init(container: container, winePosition: winePosition)
+        func winePositionDetailsViewModel(for winePosition: WinePosition) -> WinePositionDetailsView.ViewModel {
+            .init(container: container, winePosition: winePosition)
         }
 
         // MARK: - Private
 
         private func loadWinePosition(with id: String?) {
             guard let id = id else { return }
-            container.services.catalogService.load(with: id)
+            container.services.catalogService
+                .load(with: id)
                 .sinkToResult { result in
-                    // TODO: раскоментить, когда api будет работать
-//                switch result {
-//                case .success(let pos):
-//                    self.winePosition = pos
-//                case .failure(let error):
-//                    print("error:", error.localizedDescription)
-//                }
-                // TODO: удалить, когда api заработает.
-                    self.winePosition = WinePosition(id: id, title: "wineTitle", country: "country", color: .orange, year: "2012", wineSugar: .dry, quantityLiters: 0.5, isLiked: false, chemistry: 0.5, titleImageUrl: "", retailerName: "retailerName", rating: 0.3, originalPriceRub: 200, discountPercents: 10)
-            }
-            .store(in: cancelBag)
+                    switch result {
+                    case .success(let pos):
+                        self.winePosition = pos
+                    case .failure(let error):
+                        print("error:", error.localizedDescription)
+                    }
+                }
+                .store(in: cancelBag)
         }
     }
 }
