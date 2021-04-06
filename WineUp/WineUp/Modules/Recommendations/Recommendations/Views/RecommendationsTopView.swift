55fb7c60aa447033051354620a780ddbf20b6065
Nikolai Solonenko <70380928+nsolonenko@users.noreply.github.com>
2021-04-06 19:12:42 +0300

    UI 25 recommendation new elements (#94)
    * UI-25-recommendation-new-elements add new views
    
    * UI-25: Moved Recommendations views to another dir, fixed code style, fixed hardcoded size of image in top view
    
    Co-authored-by: Aleksandr Pakhomov <twofoursixeight@yandex.ru>

---
+++ WineUp/WineUp/Modules/Recommendations/Recommendations/Views/RecommendationsTopView.swift
@@ -0,0 +1,48 @@
+//
+//  RecommendationsTopView.swift
+//  WineUp
+//
+//  Created by Nikolai Solonenko on 02.04.2021.
+//
+
+import SwiftUI
+import struct Kingfisher.KFImage
+
+struct RecommendationsTopView: View {
+    var body: some View {
+        TabView {
+            imageView()
+            imageView()
+            imageView()
+            imageView()
+            imageView()
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .scaledToFit()
+    }
+
+    // MARK: Displaying Items
+
+    private func imageView() -> some View {
+        KFImage.fromUniverseUrl("https://picsum.photos/311/185")
+            .resizable()
+            .scaledToFill()
+            .cornerRadius(5.0)
+            .padding(12)
+            .background(Color.white)
+            .cornerRadius(8)
+    }
+}
+
+// MARK: - Preview
+
+#if DEBUG
+struct RecommendationsTopView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecommendationsTopView()
+            .previewLayout(.fixed(width: 414, height: 300))
+            .padding()
+            .background(Color.red)
+    }
+}
+#endif
